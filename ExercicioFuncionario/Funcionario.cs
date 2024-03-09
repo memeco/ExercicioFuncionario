841e537e6ff1b6e044793c8878c9ae827910e20c
Emerson Marques Pedro <emersonmp@gmail.com>
2024-03-09 11:26:55 -0300

    Adicionei Funcionario.cs

---
+++ ExercicioFuncionario/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿public class Funcionario
+{
+    private string nome;
+    private double salarioBruto;
+    private double imposto;
+
+    public Funcionario(string nome, double salarioBruto, double imposto)
+    {
+        this.nome = nome;
+        this.salarioBruto = salarioBruto;
+        this.imposto = imposto;
+    }
+
+    public string GetNome()
+    {
+        return nome;
+    }
+
+    public double GetSalarioBruto()
+    {
+        return salarioBruto;
+    }
+
+    public double CalcularSalarioLiquido()
+    {
+        return salarioBruto - (salarioBruto * imposto);
+    }
+
+    public void AumentarSalario(double porcentagem)
+    {
+        salarioBruto += salarioBruto * (porcentagem / 100);
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {nome}\nSalário Bruto: R${salarioBruto:F2}\nSalário Líquido: R${CalcularSalarioLiquido():F2}";
+    }
+}
